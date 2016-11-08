f56e3667753aa9997a3a548d1ae19b41a026b15d
Mario <pmario91@gmail.com>
2016-11-08 23:30:19 +0100

    Done

---
+++ app/src/main/java/mario/android/tvseries/ShowInfoActivity.java
@@ -0,0 +1,42 @@
+package mario.android.tvseries;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class ShowInfoActivity extends AppCompatActivity {
+
+    private WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_info);
+
+        mWebView = (WebView) findViewById(R.id.show_info_webview);
+
+        String tvShowUrl = getIntent().getStringExtra("tvShowUrl");
+
+        // Enable Javascript
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        // Force links and redirects to open in the WebView instead of in a browser
+        mWebView.setWebViewClient(new WebViewClient());
+
+        mWebView.loadUrl(tvShowUrl);
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(mWebView.canGoBack()) {
+            mWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
